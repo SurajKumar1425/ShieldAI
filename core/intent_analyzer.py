903368f2dd9906d6cf1cfe5a8d2ba5c3c5e98603
SurajKumar1425 <kumar813386@gmail.com>
2026-06-19 09:31:07 +0530

    Add intent analysis engine

--- core/intent_analyzer.py
+++ core/intent_analyzer.py
@@ -1,1 +1,45 @@
-# ShieldAI
+SAFE_KEYWORDS = [
+    "example",
+    "sample",
+    "dummy",
+    "test",
+    "tutorial",
+    "documentation",
+    "how to",
+    "create",
+    "learn",
+    "regex",
+    "format"
+]
+
+
+DANGEROUS_KEYWORDS = [
+    "real",
+    "customer",
+    "client",
+    "production",
+    "my account",
+    "my password",
+    "my api key",
+    "secret"
+]
+
+
+def analyze_intent(text):
+
+    text = text.lower()
+
+    safe_context = any(
+        keyword in text
+        for keyword in SAFE_KEYWORDS
+    )
+
+    dangerous_context = any(
+        keyword in text
+        for keyword in DANGEROUS_KEYWORDS
+    )
+
+    return {
+        "safe_context": safe_context,
+        "dangerous_context": dangerous_context
+    }
