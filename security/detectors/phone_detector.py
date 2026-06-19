7843119755928f6fda098719575122f613764444
SurajKumar1425 <kumar813386@gmail.com>
2026-06-19 15:44:06 +0530

    Move phone detector to correct package

---
+++ security/detectors/phone_detector.py
@@ -0,0 +1,21 @@
+import re
+
+
+def detect_phone(text: str):
+    """
+    Detect Indian phone numbers
+    """
+
+    pattern = r"\b(?:\+91[- ]?)?[6-9]\d{9}\b"
+
+    matches = re.findall(pattern, text)
+
+    if matches:
+        return {
+            "type": "PHONE_NUMBER",
+            "count": len(matches),
+            "status": "DETECTED",
+            "matches": matches
+        }
+
+    return None
